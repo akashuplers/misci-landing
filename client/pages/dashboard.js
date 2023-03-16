3998dc4f347e7448eff6fddb66feff4a7b25116e
Harsh Sahu <harshsahuhs1999@gmail.com>
2023-03-16 12:54:05 +0530

    changes

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
+import { Editor } from "@tinymce/tinymce-react";
+import DragNdrop from "../components/dragNdrop";
 
 export default function Home() {
   return (
     <>
       <Layout />
+
+      <div className="h-[100%] w-[60%] pl-[20%]">
+        <DragNdrop />
+        <Editor apiKey="qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc" />
+      </div>
     </>
   );
 }
