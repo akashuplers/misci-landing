d8c36446a2ec483901d33b69d182c7c533e2c2d3
akashuplers <akash.sharma@nowigence.com>
2023-03-27 12:50:34 +0530

    fixed interface for ideas

--- backend/interfaces.ts
+++ backend/interfaces.ts
@@ -14,6 +14,11 @@ export interface UpdateBlogMutationArg {
     platform: String;
 }
 
+export interface Ideas {
+    text: String;
+    article_id: String;
+}
+
 export interface ReGenerateBlogMutationArg {
     ideas: [Ideas];
     blog_id: string
