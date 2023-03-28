7a3da596dcf48dd8bb769843c861d50df92c4d12
Harsh Sahu <harshsahuhs1999@gmail.com>
2023-03-28 11:52:38 +0530

    changes

--- client/graphql/mutations/regenerateBlog.js
+++ client/graphql/mutations/regenerateBlog.js
@@ -1,29 +1,28 @@
 import { gql } from "@apollo/client";
 
-export const generateBlog = gql`
+export const regenerateBlog = gql`
   mutation ($options: RegenerateBlogOptions!) {
-  regenerateBlog(options: $options) {
-    _id
-    publish_data {
-      tiny_mce_data {
-        tag
-        children
+    regenerateBlog(options: $options) {
+      _id
+      publish_data {
+        tiny_mce_data {
+          tag
+          children
+        }
+        published_date
+        platform
+        published
+        creation_date
       }
-      published_date
-      platform
-      published
-      creation_date
-    }
-    ideas {
-      blog_id
       ideas {
-        used
-        idea
-        article_id
+        blog_id
+        ideas {
+          used
+          idea
+          article_id
+        }
       }
+      article_id
     }
-    article_id
   }
-}
-
 `;
