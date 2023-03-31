03216b0fbbfa87161120139a799db4b3e9a2191a
Vashisth Bhushan <vashisthbhushan@gmail.com>
2023-03-31 11:37:55 +0530

    Saved list

--- client/pages/saved.js
+++ client/pages/saved.js
@@ -1,47 +1,64 @@
+/* eslint-disable react/jsx-key */
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import styles from "../styles/saved.module.css"
-// import { getAllBlogs } from "../graphql/queries/getAllBlogs";
+import { useQuery } from "@apollo/client";
+import { getAllBlogs } from "../graphql/queries/getAllBlogs";
+import Head from "next/head";
+import { contextType } from "react-modal";
 
-export default function saved() {
+export default function Saved() {
+
+    const {data, error, loading} = useQuery(getAllBlogs,{context : { Headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },}});
         
-    const [allBlogs, setAllBlogs] = useState([
-        {
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
-            image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            tags: ["label1","label2"],
-            title: "this a multiline    filler title for saved post",
-            _id: "6423f2a5df61bee260863244"
-        },
-        {
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
-            image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            tags: ["label1","label2"],
-            title: "this a multiline    filler title for saved post",
-            _id: "6423f2a5df61bee260863244"
-        },
-        {
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
-            image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            tags: ["label1","label2"],
-            title: "this a multiline    filler title for saved post",
-            _id: "6423f2a5df61bee260863244"
-        },
-        {
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
-            image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            tags: ["label1","label2"],
-            title: "this a multiline    filler title for saved post",
-            _id: "6423f2a5df61bee260863244"
-        },
-        {
-            description: null,
-            image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            tags: ["label1","label2"],
-            title: "this a multiline    filler title for saved post",
-            _id: "6423f2a5df61bee260863244"
-        }
-    ]);
+
+    if(loading){
+        return <h1>Loading</h1>;
+}
+
+if (data){
+
+ console.log(data)
+}
+    // const [allBlogs, setAllBlogs] = useState([
+    //     {
+    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
+    //         image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //         tags: ["label1","label2"],
+    //         title: "this a multiline    filler title for saved post",
+    //         _id: "6423f2a5df61bee260863244"
+    //     },
+    //     {
+    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
+    //         image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //         tags: ["label1","label2"],
+    //         title: "this a multiline    filler title for saved post",
+    //         _id: "6423f2a5df61bee260863244"
+    //     },
+    //     {
+    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
+    //         image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //         tags: ["label1","label2"],
+    //         title: "this a multiline    filler title for saved post",
+    //         _id: "6423f2a5df61bee260863244"
+    //     },
+    //     {
+    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. ',
+    //         image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //         tags: ["label1","label2"],
+    //         title: "this a multiline    filler title for saved post",
+    //         _id: "6423f2a5df61bee260863244"
+    //     },
+    //     {
+    //         description: null,
+    //         image: "https://images.pexels.com/photos/3471423/pexels-photo-3471423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //         tags: ["label1","label2"],
+    //         title: "this a multiline    filler title for saved post",
+    //         _id: "6423f2a5df61bee260863244"
+    //     }
+    // ]);
 
     /*useEffect(() => {
         function fetchAllBlogs(){
@@ -73,16 +90,16 @@ export default function saved() {
         fetchAllBlogs()
     },[])*/
 
-    if(allBlogs == null) return
+    // if(allBlogs == null) return
 
-    console.log(allBlogs)
+    // console.log(allBlogs)
     return (
         <>
             <Layout>
                 <div className="flex divide-x">
                     <div className={styles.savedBlogContainer + " h-[100%] w-[65%] ml-[27%] mr-9"}>
                          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                            {allBlogs.map((blog) => (
+                            {/* {allBlogs.map((blog) => (
                                 <div key={blog._id} className={styles.blogContainer + " group relative"}>
                                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-[30vh]">
                                         <img
@@ -110,7 +127,7 @@ export default function saved() {
                                         </div>
                                     </div>
                                 </div>
-                            ))}
+                            ))} */}
                         </div>
                     </div>
                 </div>
