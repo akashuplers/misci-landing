6202759a10944bc46e4e0cfc385b73c102de0c43
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-04-03 13:35:36 +0530

    linkedin data incoming

--- client/constants.ts
+++ client/constants.ts
@@ -1,6 +1,3 @@
 export const WEBSOCKET_URL = "wss://maverick.lille.ai/graphql"
 export const GRAPHQL_URL = "https://maverick.lille.ai/graphql"
 
-export const LINKEDIN_CLIENT_ID = '868bordkbw6hm6';
-
-export const LINKEDIN_CLIENT_SECRET = 'kOALvAhpSOkYx4MN';
--- client/constants/apiEndpoints.js
+++ client/constants/apiEndpoints.js
@@ -9,4 +9,19 @@ export const API_ROUTES = {
   LOGIN_ENDPOINT: "/auth/user/login",
 };
 
+export const LI_API_ENDPOINTS = {
+  TW_ACCESS_TOKEN: '/auth/twitter/access-token',
+  ARTICLE: '/auth/landing/article',
+  LI_POST: '/auth/landing/linkedin/post',
+  LI_ACCESS_TOKEN: '/auth/linkedin/token',
+  LI_PROFILE: '/auth/linkedin/me',
+  TW_POST: '/auth/landing/twitter/post',
+};
+
+export const LINKEDIN_CLIENT_ID = '868bordkbw6hm6';
+
+export const LINKEDIN_CLIENT_SECRET = 'kOALvAhpSOkYx4MN';
+
 export const GQL_SUBSCRIPTION_ENDPOINT = "wss://maverick.lille.ai/graphql";
+
+
