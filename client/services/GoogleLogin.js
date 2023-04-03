62ae80b4080d8390171113ce8e87e1e2fcc6cf0d
Vashisth Bhushan <vashisthbhushan@gmail.com>
2023-04-03 16:57:20 +0530

    Fix google

--- client/services/GoogleLogin.js
+++ client/services/GoogleLogin.js
@@ -1,9 +1,8 @@
 // Import the functions you need from the SDKs you need
-import "firebase/auth"
 import { initializeApp } from "firebase/app";
-import firebase from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,33 +16,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export let firebaseAuth;
-if(firebase !== undefined){
-    firebaseAuth = firebase.auth();
-}
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const signInWithGoogle = (login, keywords, setLoading) => {
-  firebaseAuth
-    .signInWithPopup(googleProvider)
-    .then((res) => {
-      const { family_name, given_name } = res.additionalUserInfo.profile;
-      const companyNameData = `${given_name} ${family_name} ${Date.now()}`;
-      const userEmail = res.user.email;
-      userDetails = {
-        ...userDetails,
-        email: res.user.email,
-        given_name: given_name,
-        family_name: family_name,
-      };
-      login
-        ? socialLogin()
-        : createCompany(companyNameData, userEmail, keywords);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export const signUpWithGoogle = (callback) => {
+  signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      console.log(result);
+      // do something with the result
     })
     .catch((error) => {
-      setLoading?.(false);
       console.log(error.message);
     });
 };
-
-
-
