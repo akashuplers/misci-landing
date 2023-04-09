4e7b90207e6dd275e0e4e30892ea12eae856611e
Vashisth Bhushan <vashisthbhushan@gmail.com>
2023-04-09 15:04:31 +0530

    Preferences Modal

--- client/pages/dashboard/[bid].js
+++ client/pages/dashboard/[bid].js
@@ -5,10 +5,12 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import DashboardInsights from "../../components/DashboardInsights";
 import Navbar from "../../components/Navbar";
+import PreferencesModal from "../../modals/PreferencesModal";
 import TinyMCEEditor from "../../components/TinyMCEEditor";
 import { jsonToHtml } from "../../helpers/helper";
 
 export default function Post() {
+  const [pfmodal, setPFModal] = useState(false);
   const router = useRouter();
   const { bid } = router.query;
   const { data, loading, error } = useQuery(getBlogbyId, {
@@ -17,10 +19,43 @@ export default function Post() {
     },
   });
 
+var getToken;
+  if (typeof window !== "undefined") {
+    getToken = localStorage.getItem("token");
+  }
+
+
+
+  const {
+    data: meeData,
+  } = useQuery(meeAPI, {
+    context: {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + getToken,
+      },
+    },
+  });
+
+  useEffect(() => {
+    console.log(meeData)
+    if(meeData?.me.prefFilled === false){
+      setPFModal(true);
+    } 
+  },[meeData])
+
   return (
     <>
       <Layout>
         <div className="flex divide-x mt-[2em]">
+      {pfmodal && 
+        <PreferencesModal
+          pfmodal={pfmodal}
+          setPFModal={setPFModal}
+          getToken={getToken}
+        />
+
+        }
           <div className="h-[100%] w-[70%] mx-5 relative">
             <TinyMCEEditor
               editorText={jsonToHtml(
