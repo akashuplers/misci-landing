c3eaee5d8187621b075bacce740f8a9ce0b6e19a
Harsh Sahu <harshsahuhs1999@gmail.com>
2023-04-10 18:22:40 +0530

    bug-fixes-login

--- client/pages/public/[bid].js
+++ client/pages/public/[bid].js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/client";
 import { getBlogbyId } from "../../graphql/queries/getBlogbyId";
 import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
-import styles from "../../styles/publish.module.css"
+import styles from "../../styles/publish.module.css";
 
 import { jsonToHtml } from "../../helpers/helper";
 import LoaderPlane from "../../components/LoaderPlane";
@@ -11,35 +11,42 @@ import LoaderPlane from "../../components/LoaderPlane";
 export default function Post() {
   const router = useRouter();
   const { bid } = router.query;
-  const [data, setData] = useState("")
+  const [data, setData] = useState("");
 
-  const { data:gqlData, loading, error } = useQuery(getBlogbyId, {
+  const {
+    data: gqlData,
+    loading,
+    error,
+  } = useQuery(getBlogbyId, {
     variables: {
       fetchBlogId: bid,
     },
   });
 
+  if (typeof window !== "undefined") {
+    window.addEventListener("beforeunload", function (event) {
+      event.stopImmediatePropagation();
+    });
+  }
+
   useEffect(() => {
     const html = jsonToHtml(gqlData?.fetchBlog?.publish_data[2].tiny_mce_data);
-    setData(html)
-  },[router, gqlData])
+    setData(html);
+  }, [router, gqlData]);
 
   useEffect(() => {
     const publishContainer = document.getElementById("publishContainer");
-    if(publishContainer != null){
+    if (publishContainer != null) {
       publishContainer.innerHTML = data;
     }
-  },[data])
+  }, [data]);
 
-
-
-  if(loading) return <LoaderPlane/>
+  if (loading) return <LoaderPlane />;
   return (
     <>
-      <Navbar/>
-      <div className={styles.publishContainer} id="publishContainer">
-      </div>
-      <br/>
+      <Navbar />
+      <div className={styles.publishContainer} id="publishContainer"></div>
+      <br />
     </>
   );
 }
