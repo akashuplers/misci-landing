34e6258cfc74a05179844afce933cf3771d71090
Harsh Sahu <harshsahuhs1999@gmail.com>
2023-04-12 19:44:03 +0530

    upgrade-added

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -198,7 +198,7 @@ export default function Sidebar() {
                   </div>
                   <div className="flex flex-shrink-0 pb-0 pt-4">
                     <a
-                      href="#"
+                      href="/upgrade"
                       className="ml-6 inline-flex items-center rounded-md bg-[#4A3AFE] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       <svg
@@ -267,7 +267,7 @@ export default function Sidebar() {
             </div>
             <div className="flex flex-shrink-0 pb-0 pt-4">
               <a
-                href="#"
+                href="/upgrade"
                 className="ml-6 inline-flex items-center rounded-md bg-[#4A3AFE] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <svg
