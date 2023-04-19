3bf4203b5f3c3b60e892533764d95115c41f1560
Harsh Sahu <harshsahuhs1999@gmail.com>
2023-04-19 13:30:00 +0530

    changes-image-alert-searchbar

--- client/components/TinyMCEEditor.js
+++ client/components/TinyMCEEditor.js
@@ -50,6 +50,7 @@ export default function TinyMCEEditor({
   const [isCopied, setIsCopied] = useState(false);
   const [option, setOption] = useState("blog");
   const [imageURL, setImageURL] = useState("");
+  const [isalert, setAlert] = useState(false);
 
   const onCopyText = () => {
     setIsCopied(true);
@@ -432,21 +433,55 @@ export default function TinyMCEEditor({
               className="blog-toggle-button cta active wordpress flex gap-1 items-center"
               onClick={handleBlog}
             >
-              <svg style={{pointerEvents:'none'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="19px" height="19px"><path d="M 30.398438 2 L 7 2 L 7 48 L 43 48 L 43 14.601563 Z M 15 28 L 31 28 L 31 30 L 15 30 Z M 35 36 L 15 36 L 15 34 L 35 34 Z M 35 24 L 15 24 L 15 22 L 35 22 Z M 30 15 L 30 4.398438 L 40.601563 15 Z"/></svg>
+              <svg
+                style={{ pointerEvents: "none" }}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="19px"
+                height="19px"
+              >
+                <path d="M 30.398438 2 L 7 2 L 7 48 L 43 48 L 43 14.601563 Z M 15 28 L 31 28 L 31 30 L 15 30 Z M 35 36 L 15 36 L 15 34 L 35 34 Z M 35 24 L 15 24 L 15 22 L 35 22 Z M 30 15 L 30 4.398438 L 40.601563 15 Z" />
+              </svg>
               Blog
             </div>
             <div
               className="blog-toggle-button cta linkedin flex gap-1 items-center"
               onClick={handleLinkedinBlog}
             >
-              <svg style={{pointerEvents:'none'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20px" height="20px"><path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"/><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"/></svg>
+              <svg
+                style={{ pointerEvents: "none" }}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="20px"
+                height="20px"
+              >
+                <path
+                  fill="#0288D1"
+                  d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
+                />
+                <path
+                  fill="#FFF"
+                  d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
+                />
+              </svg>
               Linkedin
             </div>
             <div
               className="blog-toggle-button cta twitter flex gap-1 items-center"
               onClick={handleTwitterBlog}
             >
-              <svg style={{pointerEvents:'none'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20px" height="20px"><path fill="#03A9F4" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"/></svg>
+              <svg
+                style={{ pointerEvents: "none" }}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="20px"
+                height="20px"
+              >
+                <path
+                  fill="#03A9F4"
+                  d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"
+                />
+              </svg>
               Twitter
             </div>
           </div>
@@ -536,6 +571,32 @@ export default function TinyMCEEditor({
           </div>
         )}
       </div>
+      {isalert && (
+        <div className="p-2 text-xs">
+          Lille is not responsible for any images which are uploaded by you
+          which contains copyright infringement.
+          <button
+            onClick={() => {
+              setAlert(false);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-6 ml-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
       <Editor
         value={updatedText || editorText}
         apiKey="i40cogfqfupotdcavx74ibdbucbojjvpuzbl8tqy34atmkyd"
@@ -587,6 +648,7 @@ export default function TinyMCEEditor({
                   // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
                   var url = response.url;
                   setImageURL(url);
+                  setAlert(true);
                   console.log("response.data", response.data);
                   console.log("imageURL", imageURL);
                   console.log("88", url);
