3a169c92f1989a29b7173ba40be5853bcbf9e9fe
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-04-28 12:50:15 +0530

    pagination component and pesponse time uncomment

--- client/pages/published.js
+++ client/pages/published.js
@@ -8,6 +8,7 @@ import { getAllBlogs } from "../graphql/queries/getAllBlogs";
 import Head from "next/head";
 import { contextType } from "react-modal";
 import LoaderScan from "../components/LoaderScan";
+import Pagination from "../components/Pagination";
 import { deleteBlog } from "../graphql/mutations/deleteBlog";
 import { toast } from "react-toastify";
 import Modal from "react-modal";
@@ -54,9 +55,6 @@ export default function Saved() {
     { data: delteData, loading: delteLoading, error: delteError },
   ] = useMutation(deleteBlog);
 
-  var paginationArr = [];
-  for (var i = 1; i <= Math.ceil(data?.getAllBlogs.count / PAGE_COUNT); i++)
-    paginationArr.push(i);
 
   const files = [
     {
@@ -250,47 +248,7 @@ export default function Saved() {
             </ul>
           </>
         )}
-        {paginationArr.length > 1 && (
-          <div
-            className="pagination"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "2em",
-              paddingBottom: "2em",
-            }}
-          >
-            <ul
-              style={{
-                display: "flex",
-                gap: "2em",
-                listStyleType: "none",
-              }}
-            >
-              {paginationArr.map((el, index) => (
-                <li
-                  style={{
-                    border: "1px solid",
-                    width: "30px",
-                    height: "30px",
-                    borderRadius: "50%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    cursor: "pointer",
-                  }}
-                  className={pageSkip === index ? "active" : ""}
-                  onClick={(e) => {
-                    setPageSkip(index);
-                  }}
-                >
-                  {el}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+        <Pagination pageSkip={pageSkip} setPageSkip={setPageSkip} totalItems={data?.getAllBlogs.count}/> 
         <Modal
           isOpen={openModal}
           onRequestClose={() => setOpenModal(false)}
