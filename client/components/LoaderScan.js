261ebbb5d0fb5a03b79ea8e9353a7a2ac259acfc
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-05-05 12:37:38 +0530

    ui changes and interceptor fixes

--- client/components/LoaderScan.js
+++ client/components/LoaderScan.js
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function LoaderScan() {
   return (
-    <div className="mx-auto ml-0 pl-10 text-center flex center flex-col">
+    <div className="mx-auto ml-0 pl-10 text-center flex center flex-col relative">
       <img className="mx-auto" src="/loader.gif"></img>
-      <div className="-mt-12 animate-pulse text-sm">Loading ...</div>
+      <div className="-mt-12 animate-pulse text-sm" style={{
+        position: 'absolute',
+        bottom: '20%',
+        left: '0',
+        right: '0'
+      }}>Loading ...</div>
     </div>
   );
 }
