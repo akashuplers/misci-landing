5bd932198527d685cfd465ea3994e5a4d78b0804
akashuplers <akash.sharma@nowigence.com>
2023-05-08 20:17:12 +0530

    uncommented pricing and upgrade

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -252,7 +252,7 @@ export default function Sidebar() {
                       ))}
                     </nav>
                   </div>
-                  {/* <div className="flex flex-shrink-0 pb-0 pt-4">
+                  <div className="flex flex-shrink-0 pb-0 pt-4">
                     <Link
                       href="/upgrade"
                       className="ml-6 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -278,7 +278,7 @@ export default function Sidebar() {
                         />
                       </svg>
                     </Link>
-                  </div> */}
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
               <div className="w-14 flex-shrink-0">
