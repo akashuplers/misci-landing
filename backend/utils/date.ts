7df192cb11dbefe46ee591ccfdf215984f554df6
akashuplers <akash.sharma@nowigence.com>
2023-05-16 16:20:17 +0530

    updated api of monthly credit add

--- backend/utils/date.ts
+++ backend/utils/date.ts
@@ -56,3 +56,18 @@ export const monthDiff = (d1: Date, d2: Date) => {
   months += d2.getMonth();
   return months <= 0 ? 0 : months;
 }
+
+export const daysBetween = (first: Date, second: Date)  => {
+
+  // Copy date parts of the timestamps, discarding the time parts.
+  var one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
+  var two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
+
+  // Do the math.
+  var millisecondsPerDay = 1000 * 60 * 60 * 24;
+  var millisBetween = two.getTime() - one.getTime();
+  var days = millisBetween / millisecondsPerDay;
+
+  // Round down.
+  return Math.floor(days);
+}
