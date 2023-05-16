0c8aa49459b3ba59497b38f9faaefc4af944ef91
akashuplers <akash.sharma@nowigence.com>
2023-05-16 12:28:00 +0530

    added credit renew day and fixed citation in regenerate

--- backend/utils/date.ts
+++ backend/utils/date.ts
@@ -48,3 +48,11 @@ export function diff_minutes(dt2: Date, dt1: Date)
  return diff;
  
 }
+
+export const monthDiff = (d1: Date, d2: Date) => {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+}
