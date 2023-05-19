87d048375cc979c40bfd06e38a221cb31db1a0fb
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2023-05-19 13:00:46 +0530

    footer.tsx bug fixed

---
+++ client/components/Footer.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white  py-2 px-20 bottom-0 w-full">
+      <div className="flex items-center justify-between align-middle">
+        <div>
+          <Image
+            src="/lille_logo_light.png"
+            style={{ filter: "drop-shadow(2px 4px 6px black" }}
+            width={100}
+            height={100}
+            alt="Lille AI logo"
+          />
+        </div>
+        <div className="space-x-4 flex flex-row">
+          <a
+            href="https://twitter.com/Lille_AI"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-white transition-colors duration-300"
+          >
+            <FaTwitter size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/lille-ai/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-white transition-colors duration-300"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+      </div>
+      <div className="mt-4 text-center text-gray-400">
+        Copyright © Nowigence, Inc. -  All Rights Reserved 2023
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
