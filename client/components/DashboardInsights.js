293ea38daa8cf4a267513997c9d7d97008aad79b
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2023-05-23 18:06:47 +0530

    fixed the issue

--- client/components/DashboardInsights.js
+++ client/components/DashboardInsights.js
@@ -63,6 +63,7 @@ export default function DashboardInsights({
   const setShowContributionModal = useByMeCoffeModal((state) => state.toggleModal);
   const [toggle, setToggle] = useState(true);
   const toggleClass = " transform translate-x-3";
+  const creditLeft = useStore((state) => state.creditLeft);
 
   useEffect(() => {
     setFreshIdeas(oldFreshIdeas);
@@ -444,7 +445,9 @@ export default function DashboardInsights({
           console.log(meeData);
           const credits = meeData?.me?.credits;
           console.log('CREDITS : ' + credits);
-          const SHOW_CONTRIBUTION_MODAL = (localStorage.getItem('payment') === undefined || localStorage.getItem('payment') === null) && (localStorage.getItem('ispaid') === null || localStorage.getItem('ispaid') === undefined || localStorage.getItem('ispaid') === 'false') && (credits === 15 || credits === 5) && !meeData?.me?.isSubscribed;
+          var userCredits = meeData?.me?.totalCredits - creditLeft -1;
+          console.log('USER CREDITS: ' + userCredits);
+          const SHOW_CONTRIBUTION_MODAL = (localStorage.getItem('payment') === undefined || localStorage.getItem('payment') === null) && (localStorage.getItem('ispaid') === null || localStorage.getItem('ispaid') === undefined || localStorage.getItem('ispaid') === 'false') && (userCredits === 15 || userCredits === 5) && !meeData?.me?.isSubscribed;
           console.log('SHOW_CONTRIBUTION_MODAL: ', SHOW_CONTRIBUTION_MODAL);
           if (SHOW_CONTRIBUTION_MODAL) {
             setShowContributionModal(true);
