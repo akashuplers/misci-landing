1f4d8d6466f9343c66de4495595bcd05a598496f
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2023-05-23 17:25:54 +0530

    MADE CHANGES

--- client/components/DashboardInsights.js
+++ client/components/DashboardInsights.js
@@ -441,9 +441,10 @@ export default function DashboardInsights({
           // setRunContributionModal((prev) => prev++);
           console.log('MEE DATA');
           console.log('MEE DATA');
+          console.log(meeData);
           const credits = meeData?.me?.credits;
           console.log('CREDITS : ' + credits);
-          const SHOW_CONTRIBUTION_MODAL = (localStorage.getItem('payment') === undefined || localStorage.getItem('payment') === null) && (localStorage.getItem('ispaid') === null || localStorage.getItem('ispaid') === undefined || localStorage.getItem('ispaid') === 'false') && (credits === 19 || credits === 9) && !meeData?.me?.isSubscribed;
+          const SHOW_CONTRIBUTION_MODAL = (localStorage.getItem('payment') === undefined || localStorage.getItem('payment') === null) && (localStorage.getItem('ispaid') === null || localStorage.getItem('ispaid') === undefined || localStorage.getItem('ispaid') === 'false') && (credits === 15 || credits === 5) && !meeData?.me?.isSubscribed;
           console.log('SHOW_CONTRIBUTION_MODAL: ', SHOW_CONTRIBUTION_MODAL);
           if (SHOW_CONTRIBUTION_MODAL) {
             setShowContributionModal(true);
