cc1b684826fde9accb0c043c6417ab6609c5b289
akashuplers <akash.sharma@nowigence.com>
2023-05-30 20:18:29 +0530

    updated footer text

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,17 +1,15 @@
-import React from "react";
 import Image from "next/image";
-import { FaTwitter, FaLinkedin } from "react-icons/fa";
-
-const Footer = () => {
+import {​​​​​​ FaLinkedin, FaTwitter }​​​​​​ from "react-icons/fa";
+const Footer = () => {​​​​​​
   return (
     <footer className="bg-gray-800 text-white  py-2 px-20 bottom-0 w-full">
       <div className="flex items-center justify-between align-middle">
         <div>
           <Image
             src="/lille_logo_light.png"
-            style={{ filter: "drop-shadow(2px 4px 6px black" }}
-            width={100}
-            height={100}
+            style={​​​​​​{​​​​​​ filter: "drop-shadow(2px 4px 6px black" }​​​​​​}​​​​​​
+            width={​​​​​​100}​​​​​​
+            height={​​​​​​100}​​​​​​
             alt="Lille AI logo"
           />
         </div>
@@ -22,7 +20,7 @@ const Footer = () => {
             rel="noopener noreferrer"
             className="text-gray-400 hover:text-white transition-colors duration-300"
           >
-            <FaTwitter size={24} />
+            <FaTwitter size={​​​​​​24}​​​​​​ />
           </a>
           <a
             href="https://www.linkedin.com/company/lille-ai/"
@@ -30,15 +28,14 @@ const Footer = () => {
             rel="noopener noreferrer"
             className="text-gray-400 hover:text-white transition-colors duration-300"
           >
-            <FaLinkedin size={24} />
+            <FaLinkedin size={​​​​​​24}​​​​​​ />
           </a>
         </div>
       </div>
       <div className="mt-4 text-center text-gray-400">
-        © 2023 Nowigence, Inc.
+        Copyright © Nowigence, Inc. -  All Rights Reserved 2023
       </div>
     </footer>
   );
-};
-
+}​​​​​​;
 export default Footer;
