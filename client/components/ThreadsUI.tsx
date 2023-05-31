3e99a5f5a1c2ab62c551c53080a3bfd53512f9df
Arin Prajapati <arinprajapati78@gmail.com>
2023-05-31 14:26:49 +0530

    improve the UI for threads

--- client/components/ThreadsUI.tsx
+++ client/components/ThreadsUI.tsx
@@ -85,7 +85,10 @@ const Thread = ({
       };
     
       const animationStyle = index < threadData.length - 1 ? moveUpAnimation : moveDownAnimation;
-    
+      const [isEditing, setIsEditing] = useState(false);
+      const handleEdit = () => {
+        setIsEditing(true);
+      }
     
     return (
         <div key={index} className="relative w-full min-w-[400px] thread-item"
@@ -110,7 +113,7 @@ const Thread = ({
                         placeholder="Type your thread here..."
                         value={thread}
                         minLength={1}
-                        onChange={(e) => updateTextArea(index, e.target.value)}
+                        onChange={(e) =>{ updateTextArea(index, e.target.value); handleEdit();}}
                     />
                 </div>
                 <div className={`w-[10%] flex flex-col justify-around items-center`}>
@@ -136,6 +139,30 @@ const Thread = ({
                             </svg>
                         </button>
                     )}
+                    {
+                        isEditing && (
+                            <button
+                            className="text-red-700 hover:text-red-900 flex items-center justify-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+
+                            onClick={() => deleteThread(index)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                />
+                            </svg>
+                        </button>
+                        )
+                    }
                     {index > 0 && (
                         <button
                         className="text-gray-700 hover:text-gray-900 flex items-center justify-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
