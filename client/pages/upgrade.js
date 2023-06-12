dbfe595103721dcbb3d703e9522e9066d9b7599c
Abhay Prajapati <=>
2023-06-12 14:47:56 +0530

    DONE WITH TWITTER YELLOW ALERT

--- client/pages/upgrade.js
+++ client/pages/upgrade.js
@@ -4,9 +4,9 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import { useEffect, useLayoutEffect, useState } from "react";
 import CheckoutFormUpgrade from "../components/CheckoutFormUpgrade";
+import { FeaturesItem, UpgradeFeatures } from "../components/FeatureItem";
 import Layout from "../components/Layout";
 import { API_BASE_PATH } from "../constants/apiEndpoints";
-import { FeaturesItem, UpgradeFeatures } from "../components/FeatureItem";
 
 export default function Upgrade() {
   const stripePromise = loadStripe(STRIPE_PROMISE);
