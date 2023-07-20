b8703ca74280363cefe75a9af4ecb0597ab9b0bf
theabhayprajapati <theabhayprajapati@gmail.com>
2023-07-20 18:09:31 +0530

    chagnes for loaidng

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -699,18 +699,18 @@ export function UserSaveTime(data, dataLoading) {
         <Menu.Button className="relative inline-flex w-full justify-center gap-x-1.5 rounded-md bg-indigo-100 px-3 py-2 text-sm font-semibold text-gray-900 hover:opacity-75 border-indigo-600 border-l-8  outline-white">
           {/* small width verticial line */}
           <ClockIcon className="h-5 w-5" aria-hidden="true" />
-          <span className="font-light">Time Saved for the {selectedOption}</span> {dataLoading ==true ? <ReactLoading
+          <span className="font-light">Time Saved for the {selectedOption}</span> {  data == null || dataLoading ==true ? <ReactLoading
             width={25}
             height={25}
             round={true}
           /> :
-            `- ${data[selectedOption]?.hours} h : ${data[selectedOption]?.minutes} m`}
+            data && `- ${data[selectedOption]?.hours} h : ${data[selectedOption]?.minutes} m`}
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
 
 {
-  dataLoading == false && 
+  dataLoading == false && data &&
   <Transition
     as={Fragment}
     enter="transition ease-out duration-100"
@@ -722,7 +722,7 @@ export function UserSaveTime(data, dataLoading) {
   >
     <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
       <div className="py-1">
-        {
+        { 
           Object.keys(data).map((key, index) => {
             return (
               <Menu.Item key={index}>
