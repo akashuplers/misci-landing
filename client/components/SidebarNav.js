9950af98f94c507ecf8bb9d7fc8a6fb11a62cfc5
theabhayprajapati <theabhayprajapati@gmail.com>
2023-07-20 17:59:40 +0530

    changes

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -709,48 +709,51 @@ export function UserSaveTime(data, dataLoading) {
         </Menu.Button>
       </div>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            {
-              Object.keys(data).map((key, index) => {
-                return (
-                  <Menu.Item key={index}>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                        onClick={() => {
-                          setSelectedOption(key);
-                          console.log(key);
-                          console.log(data[key]);
-                        }}
-                      >
-                        <div className="flex text-indigo-600 justify-between items-center">
-                          <div>{key}</div>
-                          <div>{data[key].seconds}</div>
-                        </div>
-                      </a>
+{
+  dataLoading == false && 
+  <Transition
+    as={Fragment}
+    enter="transition ease-out duration-100"
+    enterFrom="transform opacity-0 scale-95"
+    enterTo="transform opacity-100 scale-100"
+    leave="transition ease-in duration-75"
+    leaveFrom="transform opacity-100 scale-100"
+    leaveTo="transform opacity-0 scale-95"
+  >
+    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <div className="py-1">
+        {
+          Object.keys(data).map((key, index) => {
+            return (
+              <Menu.Item key={index}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
                     )}
-                  </Menu.Item>
-                )
-              })
-            }
+                    onClick={() => {
+                      setSelectedOption(key);
+                      console.log(key);
+                      console.log(data[key]);
+                    }}
+                  >
+                    <div className="flex text-indigo-600 justify-between items-center">
+                      <div>{key}</div>
+                      <div>{data[key]?.seconds}</div>
+                    </div>
+                  </a>
+                )}
+              </Menu.Item>
+            )
+          })
+        }
 
-          </div>
-        </Menu.Items>
-      </Transition>
+      </div>
+    </Menu.Items>
+  </Transition>
+}
     </Menu>
   )
 }
