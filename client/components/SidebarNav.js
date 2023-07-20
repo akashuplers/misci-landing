306dc424f565799217f6c8418bd2b0bd535ae90a
theabhayprajapati <theabhayprajapati@gmail.com>
2023-07-20 18:16:11 +0530

    changes

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -690,6 +690,7 @@ export default function Sidebar() {
 export function UserSaveTime(data, dataLoading) {
   // const [selectedOption, setSelectedOption] = useState(data[);
   // firstdata
+  console.log(data.data);
   data = data.data;
   const [selectedOption, setSelectedOption] = useState('Day');
 
@@ -699,7 +700,7 @@ export function UserSaveTime(data, dataLoading) {
         <Menu.Button className="relative inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-indigo-100 px-3 py-2 text-sm font-semibold text-gray-900 hover:opacity-75 border-indigo-600 border-l-8  outline-white">
           {/* small width verticial line */}
           <ClockIcon className="h-5 w-5" aria-hidden="true" />
-          <span className="font-light">Time Saved for the {selectedOption}</span> {  data == null || dataLoading ==true ? <ReactLoading
+          <span className="font-light">Time Saved for the {selectedOption}</span> {data == null || dataLoading == true ? <ReactLoading
             width={25}
             height={25}
             round={true}
@@ -709,17 +710,17 @@ export function UserSaveTime(data, dataLoading) {
         </Menu.Button>
       </div>
 
-{
-  dataLoading == false && data &&
-  <Transition
-    as={Fragment}
-    enter="transition ease-out duration-100"
-    enterFrom="transform opacity-0 scale-95"
-    enterTo="transform opacity-100 scale-100"
-    leave="transition ease-in duration-75"
-    leaveFrom="transform opacity-100 scale-100"
-    leaveTo="transform opacity-0 scale-95"
-  >
+      {
+        (dataLoading == false ||  data !== null) &&
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
     <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
       <div className="py-1">
         { 
