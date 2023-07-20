af62fcf522b214082f4c004c8c463d0fe7cf0325
theabhayprajapati <theabhayprajapati@gmail.com>
2023-07-20 17:56:40 +0530

    changes with loader

--- client/components/SidebarNav.js
+++ client/components/SidebarNav.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import ReactLoading from "react-loading";
 import useStore, {
   useBlogDataStore,
   useTabOptionStore,
@@ -529,7 +530,7 @@ export default function Sidebar() {
                   }}
                 >
                   {
-                    userTimeSaveLoading != true && meeData?.me?.totalCredits && <UserSaveTime data={userTimeSave} />
+                    meeData?.me?.totalCredits  && <UserSaveTime data={userTimeSave} dataLoading={userTimeSaveLoading} />
                   }
                   {!meeLoading && (
                     <div
@@ -686,7 +687,7 @@ export default function Sidebar() {
 }
 
 
-export function UserSaveTime(data) {
+export function UserSaveTime(data, dataLoading) {
   // const [selectedOption, setSelectedOption] = useState(data[);
   // firstdata
   data = data.data;
@@ -696,9 +697,14 @@ export function UserSaveTime(data) {
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="relative inline-flex w-full justify-center gap-x-1.5 rounded-md bg-indigo-100 px-3 py-2 text-sm font-semibold text-gray-900 hover:opacity-75 border-indigo-600 border-l-8  outline-white">
-{/* small width verticial line */}
+          {/* small width verticial line */}
           <ClockIcon className="h-5 w-5" aria-hidden="true" />
-          <span className="font-light">Time Saved for the {selectedOption}</span> {`- ${data[selectedOption]?.hours} h : ${data[selectedOption]?.minutes} m`}
+          <span className="font-light">Time Saved for the {selectedOption}</span> {dataLoading ==true ? <ReactLoading
+            width={25}
+            height={25}
+            round={true}
+          /> :
+            `- ${data[selectedOption]?.hours} h : ${data[selectedOption]?.minutes} m`}
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
