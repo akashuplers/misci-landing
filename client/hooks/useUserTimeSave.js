4289ac69f342b3395a049005e9b3feed1f8d3141
theabhayprajapati <theabhayprajapati@gmail.com>
2023-07-21 17:09:35 +0530

    fixed changes

--- client/hooks/useUserTimeSave.js
+++ client/hooks/useUserTimeSave.js
@@ -1,6 +1,49 @@
-import { useUserTimeSaveStore } from "@/store/store";
+import { useState, useEffect } from 'react';
+import {getTimeObject} from '../helpers/helper';
+import { API_BASE_PATH, API_ROUTES } from '../constants/apiEndpoints';
+const useUserTimeSave = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  const fetchData = async () => {
+    const URL = API_BASE_PATH + API_ROUTES.GET_SAVED_TIME
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'));
+      try {
+        const response = await fetch(URL, {
+            method: 'GET',
+            headers: headers,
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
 
-export const useUserTimeSave = () => {
-  const { userTimeSave, loading, error, refreshData } = useUserTimeSaveStore();
-  return { userTimeSave, loading, error, refreshData };
+        const jsonData = await response.json();
+        console.log(jsonData);
+        const improvedData = {
+            Day: getTimeObject(jsonData.data.oneDaySavedTime),
+            Week: getTimeObject(jsonData.data.oneWeekSavedTime),
+            Month: getTimeObject(jsonData.data.oneMonthSavedTime)
+          };
+          console.log('IMRPOVED DATA', improvedData)
+        setData(improvedData);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    useEffect(() => {
+      fetchData();
+      setRefresh(false); // Reset refresh to false after data is fetched
+    }, [refresh]);
+    const handleManualRefresh = () => {
+      setRefresh(true);
+    };
+
+  return { userTimeSave : data, loading, error, handleManualRefresh };
 };
+
+export default useUserTimeSave;
