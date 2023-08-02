dad09c5bd41cf4a7814eeee8d71ba73464da71c3
theabhayprajapati <theabhayprajapati@gmail.com>
2023-08-02 17:06:42 +0530

    changes for gb modal

---
+++ client/modals/GoogleDriveModal.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react"
+import Modal from "react-modal"
+
+const SCREENS_FOR_GD = {
+    'MAIN': 0,
+    'THANK_YOU': 1,
+}
+interface Props { showModal: boolean, setShowModal: (showModal: boolean) => void }
+export default function GoogleDriveModal({ showModal, setShowModal} : Props) {
+    const [screen, setScreen] = useState(SCREENS_FOR_GD.MAIN);
+    useEffect(() => { 
+        return () => {
+            setScreen(SCREENS_FOR_GD.MAIN);
+        }
+    }, [])
+    return (
+        <Modal
+        isOpen={showModal}
+        onRequestClose={() =>{
+            setShowModal(false);
+            setScreen(SCREENS_FOR_GD.MAIN);
+        }}
+        ariaHideApp={false}
+        className="modalModalWidth sm:w-[38%] max-h-[95%]"
+        style={{
+            overlay: {
+                backgroundColor: "rgba(0,0,0,0.5)",
+                zIndex: "9999",
+            },
+            content: {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                border: "none",
+                background: "white",
+                // boxShadow: "0px 4px 20px rgba(170, 169, 184, 0.1)",
+                borderRadius: "8px",
+                // width: "100%",
+                bottom: "",
+                zIndex: "999",
+                maxWidth: '55%',
+                width: '100%',
+                marginRight: "-50%",
+                height: '55%',
+                transform: "translate(-50%, -50%)",
+                padding: "30px",
+                paddingBottom: "0px",
+            },
+        }}
+    >
+        {
+            screen == SCREENS_FOR_GD.MAIN ? 
+            <div className='flex items-center justify-center'>
+                        <img className="w-[232.75px] h-[178.29px] max-w-[50%] max-h-[70%]" src="./gdrive.gif" style={{
+                            maxWidth: '50%',
+                            maxHeight: '70%'
+                        }}  />
+                        <div className="w-full h-full p-6 bg-white rounded-lg flex-col justify-center items-start gap-8 inline-flex">
+                            <div className="text-center text-black text-[19px] font-bold leading-normal">Paste URL of Google drive link. Lille will notify on your email</div>
+                            <div className="w-full h-full flex-col justify-start items-start gap-6 flex">
+                                <div className="self-stretch h-full flex-col justify-start items-start gap-1 flex">
+                                    <div className="self-stretch opacity-50 text-black text-sm font-normal">Paste URL</div>
+                                    <input className="self-stretch h-11 p-5 bg-white rounded-lg border border-neutral-200 flex-col justify-center items-start gap-[15px] flex" placeholder="e.g. https://drive.google.com/drive/folders/1UJ7T8n2Ql6q6nX7Xzg2F0Z4S7QZl5Yt2" />
+
+                                </div>
+                                <div className="self-stretch h-full flex-col justify-start items-start gap-1 flex">
+                                    <div className="self-stretch opacity-50 text-black text-sm font-normal"> Enter Email</div>
+                                    <div className="self-stretch h-11 flex-col justify-start items-start gap-1 flex">
+                                        <input className="self-stretch h-11 p-5 bg-white rounded-lg border border-neutral-200 flex-col justify-center items-start gap-[15px] flex" placeholder="e.g. Kiransingl434a@gmail.com" />
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="self-stretch h-full px-4 py-1.5 bg-indigo-600 rounded-lg flex-col justify-center items-center gap-2.5 flex"
+                            onClick={
+                                () => {
+                                    setScreen(SCREENS_FOR_GD.THANK_YOU)
+                                }
+                            }
+                            >
+                                <div className="justify-center items-center inline-flex">
+                                    <div className="px-1 justify-start items-center gap-2.5 flex">
+                                        <span className="text-center text-white text-sm font-bold leading-normal">Submit</span>
+                                    </div>
+                                </div>
+                            </button>
+                        </div>
+            </div> : 
+            <>
+                        <div className="w-full h-full px-6 py-10 bg-white rounded-lg flex-col justify-center items-center gap-10 inline-flex">
+                            <img className="w-[232.75px] h-[178.29px]" src="./thanku.gif" />
+                            <div className="self-stretch text-center text-black text-[19px] font-medium leading-normal">Thank you! Lille Team will mail back with a creative blog url</div>
+                        </div>
+            </>
+        }
+    </Modal>
+    )
+}
