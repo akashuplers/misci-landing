59eb7000dafa7719ce244d4f33b6dc6923e719e9
theabhayprajapati <theabhayprajapati@gmail.com>
2023-08-04 19:40:32 +0530

    changes

--- client/pages/public/[bid].tsx
+++ client/pages/public/[bid].tsx
@@ -100,10 +100,7 @@ export default function Post() {
     const aa = gqlData?.fetchBlog?.publish_data.find((pd) => pd.platform === "wordpress"
     ).tiny_mce_data;
     const html = jsonToHtml(aa);
-    console.log("ADD");
-    console.log(aa?.children[0].children[0].children[0]);
-    setBlogTitle(aa?.children[0].children[0].children[0])
-    setData(html);
+    console.log("ADD")
   }, [router, gqlData]);
 
   useEffect(() => {
