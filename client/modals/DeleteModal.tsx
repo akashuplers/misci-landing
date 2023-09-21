7286ce9d94e97312bfc52e9e864358d111e57fa4
= <theabhayprajapati@gmail.com>
2023-09-21 16:00:01 +0530

    fixed btn color

--- client/modals/DeleteModal.tsx
+++ client/modals/DeleteModal.tsx
@@ -63,7 +63,7 @@ function DeleteModal(props: IDeleteModal) {
             Cancel
           </button>
           <button
-            className="border-red-500 ring-1 focus:ring-1 ring-red-500 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            className="border-red-500 ring-1 focus:ring-1 ring-red-500 bg-red-300 hover:bg-red-500 text-white px-4 py-2 rounded"
             onClick={() => props.onDelete(props.data)}
           >
             Delete
