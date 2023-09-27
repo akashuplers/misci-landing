d8bddd14d01774baf505caddc4b4df1c1f30c969
vb-adg <vashisth.bhushan@techneto.ai>
2023-09-27 19:34:33 +0530

    edit

--- client/pages/pricing.js
+++ client/pages/pricing.js
@@ -277,7 +277,7 @@ export default function Pricing() {
           <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src=""
+              src="https://www.globenewswire.com/content/logo/color.svg"
               alt="Transistor"
               width={158}
               height={48}
