e88cf6303a9115c34f1cb21fc4e072c697538977
akashuplers <akash.sharma@nowigence.com>
2023-10-26 14:54:04 +0530

    added logic to add more than 1 sources in misci report

--- backend/utils/encode.ts
+++ backend/utils/encode.ts
@@ -1,3 +1,13 @@
 export const encodeURIfix = (str: any) => {
     return encodeURIComponent(str).replace(/!/g, '%21');
 }
+
+
+export const isJsonString = (str: string) => {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
