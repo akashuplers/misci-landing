428a5a54240f186c0fb0ec408be77c16fbb0e2b2
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-10-27 14:02:22 +0530

    misci all mvp done

--- client/components/MisciSwiperPage.tsx
+++ client/components/MisciSwiperPage.tsx
@@ -15,11 +15,17 @@ const MisciSwiperPage: React.FC<Props> = ({
 }) => {
   return (
     <div className='rounded-xl shadow-2xl shadow-gray-300 text-black w-full h-full flex flex-col justify-between items-start gap-4 p-6 bg-white'>
-        <h1 className='text-3xl md:text-4xl font-semibold'>{question}</h1>
-        <h4 className='text-base'>Ans: {answer}</h4>
-        <p className='text-base'>{reasoning}</p>
-        <div className='w-full h-1/3 md:h-1/2'>
-            <img className='w-full h-full max-h-[300px] object-cover rounded-lg' src={img}/>
+        <div className='flex flex-col justify-between items-start gap-4'>
+          <h1 className='text-3xl md:text-4xl font-semibold'>{question}</h1>
+          <h4 className='text-base'>Ans: {answer}</h4>
+          <p className='text-base'>{reasoning}</p>
+        </div>
+        <div className='w-full h-max rounded-lg' style={{
+          background: `url(${img}) center no-repeat`,
+          backgroundSize: 'cover',
+          flex: '1 1 fit-content'
+        }}>
+            {/* <img className='w-full object-cover rounded-lg' src={img}/> */}
         </div>
     </div>
   );
